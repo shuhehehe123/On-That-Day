a3bab7fb2b0f2ff11e8652f7caec089b5c4503db
shuhehehe123 <hjh-speaker@qq.com>
2021-02-04 21:23:04 +0800

    增加显示日期

---
+++ src/components/Calender.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="centre">
+    <h1>今天是{{ year }}年{{ month }}月{{ date }}日</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Calender",
+  data: function (){
+    return {
+      year: new Date().getFullYear(),
+      month: (new Date().getMonth())+1,
+      date: new Date().getDate()
+    };
+  },
+  /*
+  props:{
+    date:new Date()
+  }
+
+  */
+}
+</script>
+
+<style scoped>
+.centre{
+  text-align:center;
+}
+</style>
